e60f13192c38eea9b318af7d428f56d47f201773
Sc3m0r <Crush3r99@web.de>
2021-01-12 07:11:07 +0100

    Cleaning for new gitignore policy

--- app/src/main/java/bigchris/studying/androidspeechtest/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package bigchris.studying.androidspeechtest
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-
-}
--- app/src/main/java/bigchris/studying/androidspeechtest/directspeechrecognition/DirectSpeechRecognitionViewModel.kt
+++
@@ -1,7 +0,0 @@
-package bigchris.studying.androidspeechtest.directspeechrecognition
-
-import androidx.lifecycle.ViewModel
-
-class DirectSpeechRecognitionViewModel : ViewModel() {
-
-}
